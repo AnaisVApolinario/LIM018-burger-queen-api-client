5bbda888715d165b7a3232550bc319d7d89abea7
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-10-12 12:37:04 -0500

    modificando login

--- burger-api-client/src/app/view/login/login.component.ts
+++ burger-api-client/src/app/view/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
-// import { auth } from '../services/api';
+//import axios from 'axios';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+//import { auth } from '../services/api';
 
-const baseUrl:string = "http://localhost:3000/auth";
+const baseUrl: string = "http://localhost:3000/auth";
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,33 @@ const baseUrl:string = "http://localhost:3000/auth";
   styleUrls: ['./login.component.css']
 })
 
-
 export class LoginComponent implements OnInit {
-  enviar(event: Event): void {
-    event.preventDefault();
-    console.log('Event ->', event)
-    console.log('Otra pagina')
-  }
-  // constructor() { }
+  loginForm = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
+  constructor() { }
 
   ngOnInit(): void {
-    console.log('AXIOS',axios.get(baseUrl));
+    //console.log('AXIOS', axios.get(baseUrl));
   }
+
+  onLogin(form: any) {
+
+    console.log(form)
+  }
+
+
+  // export class LoginComponent implements OnInit {
+  //   enviar(event: Event): void {
+  //     event.preventDefault();
+  //     console.log('Event ->', event)
+  //     console.log('Otra pagina')
+  //   }
+  //   // constructor() { }
+
+
 
 }
 
