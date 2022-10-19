62c41ed87ed11db18c78c6241aa8727558eff4c6
roxana lopez <lopezbedonroxana@gmail.com>
2022-10-18 20:01:41 -0500

    button de menú implementado

--- burger-api-client/src/app/view/waiter/menu/almuerzo.component.ts
+++ burger-api-client/src/app/view/waiter/menu/almuerzo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Products, DataService } from 'src/app/services/data.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-almuerzo',
   templateUrl: './almuerzo.component.html',
@@ -8,15 +8,35 @@ import { Products, DataService } from 'src/app/services/data.service';
 })
 export class AlmuerzoComponent implements OnInit {
   products: Products[] = [];
-
-  constructor( private readonly dataSVc: DataService) { }
-
-  ngOnInit(): void {
-    //actulizar datos de verdad aquí
-    this.dataSVc.getProducts()
-    .subscribe( items => { //response
-      this.products = [...items];
-    });
+  seleccionDeTipo: String = ' ';
+  asignarTipoComida = (tipo: String ) => {
+    const tipoComida: String = tipo.toLowerCase().trim();
+    return tipoComida === "almuerzo" ? "almuerzo" : "desayuno";
   }
 
+
+  constructor( private dataSVc: DataService, private router:Router) { }
+  // ngOnInit(): void {
+    //   this.dataSVc.asignarTipoComida()
+    //     .subscribe(data=>{
+      //       this.empleados = data[0];
+      //     })
+      // }
+      ngOnInit(): void {
+        //actulizar datos de verdad aquí
+        this.dataSVc.getProducts()
+        .subscribe( items => { //response´
+          // console.log(items.type)
+          mostrarOcultar= () => {
+
+          }
+      this.products = [...items]
+      let filteredProducts = this.products;
+    //   if (this.productTypeSelected !== 'all') {
+    //     filteredProducts = items.filter(p => p.type === this.productTypeSelected)
+    //   }
+    //   this.filteredProducts = filteredProducts;
+    // });
+  })
+}
 }
