11d0c4757129b4b52d8a16da182d83c3800750af
Anais <avalverdea54@gmail.com>
2022-10-24 10:10:27 -0500

    llenando el componente editar con los datos del usuario

--- burger-api-client/src/app/view/managed/editar/editar.component.ts
+++ burger-api-client/src/app/view/managed/editar/editar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router'
+import { EmpleadoI } from 'src/app/modelos/empleado.interface';
 import { empleadoService } from 'src/app/services/empleados.service';
 
 @Component({
@@ -8,18 +10,38 @@ import { empleadoService } from 'src/app/services/empleados.service';
   styleUrls: ['./editar.component.css']
 })
 export class EditarComponent implements OnInit {
+  constructor(private activeroute: ActivatedRoute, private router: Router, private empleadoSvc: empleadoService) { }
 
-  constructor(private activeroute:ActivatedRoute, private router:Router, private empleadoSvc:empleadoService) { }
+  datosEmpleado!: EmpleadoI;
+  editarForm = new FormGroup({
+    nombre: new FormControl(''),
+    email: new FormControl(''),
+    empleadoId: new FormControl(''),
+    password: new FormControl(''),
+    rol: new FormControl(''),
+
+  });
+
+
 
   ngOnInit(): void {
     let empleadoid = this.activeroute.snapshot.paramMap.get('id');
     // let token = this.getToken();
-    this.empleadoSvc.getSingleEmpleado(empleadoid).subscribe(data => {
+    this.empleadoSvc.getSingleEmpleado(empleadoid).subscribe((data:any)=> {
+      this.datosEmpleado = data[0];
       console.log(data)
+      this.editarForm.setValue({
+        'nombre': this.datosEmpleado.nombre,
+        'email': this.datosEmpleado.email,
+        'empleadoId': this.datosEmpleado.id,
+        'password': this.datosEmpleado.password,
+        'rol': this.datosEmpleado.rol,
+      });
+      console.log(this.editarForm.value)
     })
   }
 
-  getToken(){
+  getToken() {
     return localStorage.getItem('token');
   }
 }
